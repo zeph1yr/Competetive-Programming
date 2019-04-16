bc90fd6a1547ed8c83a0b8ea947a28623de4f10d
Bhuwan Chandra <41923169+zeph1yr@users.noreply.github.com>
2019-04-17 03:45:22 +0530

    Counting to Infinity

---
+++ C++_playlist (Australian Informatics)/counting.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  //freopen("countin.txt", "r", stdin);
+	//freopen("countout.txt", "w", stdout);
+
+    int N;
+    cin >> N;
+    for(int x=1; x<=N; ++x)
+    {
+        cout << x <<endl;
+    }
+	return 0;
+}
