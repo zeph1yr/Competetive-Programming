33e2ecd3a436e1ac98b9a759a1c49111219d6c3d
Bhuwan Chandra <41923169+zeph1yr@users.noreply.github.com>
2019-04-17 03:26:12 +0530

    Addition.cpp

---
+++ C++_playlist (Australian Informatics)/addition.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+#include <iostream>
+using namespace std;
+int main() {
+	//freopen("addin.txt", "r", stdin);
+	//freopen("addout.txt", "w", stdout);
+	unsigned long int a,b;
+	cin >> a >> b;
+	cout << endl;
+	cout << a+b;
+	return 0;
+}
