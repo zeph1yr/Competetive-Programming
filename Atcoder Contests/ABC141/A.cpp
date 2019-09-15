e0db4f8c206618dc4ae4fe5492d74558013e76c0
Bhuwan Chandra <41923169+zeph1yr@users.noreply.github.com>
2019-09-15 19:28:42 +0530

    Rename Atcoder Contests/ABC140/A.cpp to Atcoder Contests/ABC141/A.cpp

---
+++ Atcoder Contests/ABC141/A.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  string s;
+  cin>>s;
+  if(s=="Sunny")cout<<"Cloudy";
+  else if(s=="Cloudy")cout<<"Rainy";
+  else if(s=="Rainy")cout<<"Sunny";
+}
