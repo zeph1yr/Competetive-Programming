aa057fa09eda63ed583e290a5c3cd49bccf67f75
Bhuwan Chandra <41923169+zeph1yr@users.noreply.github.com>
2020-05-24 12:48:58 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -13,13 +13,14 @@ using namespace std;
 #define f3(b) for(int k=0;k<b;k++)
 #define endl '\n'
 #define pi pair<int,int>
-#define INF 1e+18
+#define INF 1000000000000000001
 
-long long pow(long long x, long long y){  if (y == 0) return 1; else if (y % 2 == 0) return pow(x, y / 2) * pow(x, y / 2);else return x * pow(x, y / 2) * pow(x, y / 2);}
+long long pow(long long x, long long y){ if (y == 0) return 1; else if (y % 2 == 0) return pow(x, y / 2) * pow(x, y / 2);else return x * pow(x, y / 2) * pow(x, y / 2);}
+long long dig(long long n) return {floor(log10(n) + 1);}
 
 int main()
 {
 	io
 	#define int long long
-    
+
 }
